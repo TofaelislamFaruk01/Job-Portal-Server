d8f5efa569a74f4b8d9cef7e51b8107ff51bde1b
Tofael Islam Faruk <tofaelfaruk@gmail.com>
2025-05-17 17:32:26 +0600

    check 5

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 const express = require('express');
 const cors = require('cors');
-const serverless = require('serverless-http');
 require('dotenv').config();
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 3000;
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://job-portal-70de2.web.app'
+];
+
 const corsOptions = {
-  origin: 'https://job-portal-70de2.web.app',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true); // allow the request
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 };
 
+
 app.use(cors(corsOptions));
+// app.use(cors());
 app.use(express.json());
 
-// MongoDB setup
+
+
+
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eixcuzu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,66 +44,94 @@ const client = new MongoClient(uri, {
   }
 });
 
-let jobsCollection;
-let jobApplicationCollection;
-
-async function connectToDB() {
+async function run() {
   try {
-    await client.connect();
-    const db = client.db("JobPortal");
-    jobsCollection = db.collection("Jobs");
-    jobApplicationCollection = db.collection("job-applications");
-    console.log("Connected to MongoDB");
-  } catch (err) {
-    console.error("MongoDB connection error:", err);
+    // Connect the client to the server	(optional starting in v4.7)
+   // await client.connect();
+    // Send a ping to confirm a successful connection
+   // await client.db("admin").command({ ping: 1 });
+    //console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const database = client.db("JobPortal");
+   
+    const jobsCollection = database.collection("Jobs");
+
+    const jobApplicationCollection =  database.collection("job-applications");
+
+   
+
+    app.get('/jobs',async(req,res)=>{
+
+        const cursor = jobsCollection.find();
+
+        const result = await cursor.toArray(); 
+        res.send(result);
+    })
+
+    app.get('/jobs/:id',async(req,res)=>{
+
+      const id = req.params.id;
+      const query ={ _id : new ObjectId(id)}
+
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+
+    })
+
+    //job application api
+    //get api for getting all data.get one data, get some data [0,1,many] for application
+    app.get('/job-applications',async(req,res)=>{
+
+      const email = req.query.email;
+      const query = {applicant_email:email}
+      const result = await jobApplicationCollection.find(query).toArray();
+
+      //not the best way 
+      for(const applicaion of result)
+      {
+        console.log(applicaion.job_id);
+        const query1 ={ _id : new ObjectId(applicaion.job_id)}
+
+        const job = await jobsCollection.findOne(query1);
+        if(job){
+          applicaion.title = job.title;
+          applicaion.company = job.company;
+          applicaion.company_logo = job.company_logo;
+        }
+      }
+      res.send(result);
+
+    })
+    //post api for applicaion
+    app.post('/job-applications',async(req,res)=>{
+      const application = req.body;
+      const result =await jobApplicationCollection.insertOne(application);
+      res.send(result);
+    })
+
+
+    
+   // await client.db("admin").command({ ping: 1 });
+   // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+   // await client.close();
   }
 }
-connectToDB();
+run().catch(console.dir);
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('Job is falling from the sky');
-});
 
-app.get('/jobs', async (req, res) => {
-  const result = await jobsCollection.find().toArray();
-  res.send(result);
-});
 
-app.get('/jobs/:id', async (req, res) => {
-  const result = await jobsCollection.findOne({ _id: new ObjectId(req.params.id) });
-  res.send(result);
-});
+app.get('/',(req,res)=>{
+    res.send('Job is falling from the sky');
+})
 
-app.get('/job-applications', async (req, res) => {
-  const email = req.query.email;
-  const result = await jobApplicationCollection.find({ applicant_email: email }).toArray();
+app.listen(port,()=>{
+    console.log(`Job is waiting at : ${port}`);
+})
 
-  for (const app of result) {
-    const job = await jobsCollection.findOne({ _id: new ObjectId(app.job_id) });
-    if (job) {
-      app.title = job.title;
-      app.company = job.company;
-      app.company_logo = job.company_logo;
-    }
-  }
-
-  res.send(result);
-});
-
-app.post('/job-applications', async (req, res) => {
-  const application = req.body;
-  const result = await jobApplicationCollection.insertOne(application);
-  res.send(result);
-});
-
-// ✅ LOCAL DEV: Enable server if not running in serverless
-// if (process.env.NODE_ENV !== 'production') {
-//   const port = process.env.PORT || 3000;
-//   app.listen(port, () => {
-//     console.log(`Server running locally at http://localhost:${port}`);
-//   });
-// }
-
-// ✅ VERCEL: Export for serverless
-module.exports.handler = serverless(app);
+module.export=app;
+// const serverless = require('serverless-http');
+// module.exports.handler = serverless(app);
